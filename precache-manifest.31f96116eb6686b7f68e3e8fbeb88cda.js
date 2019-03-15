aa00a2b4e0d24fd0dbbeee6f9fe71596ef56bc1f
Valentin <kaniwets.valentin@yandex.ru>
2019-03-15 12:11:23 +0300

    Updates

---
+++ precache-manifest.31f96116eb6686b7f68e3e8fbeb88cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a9ba93f9acd24c36e97",
+    "url": "/static/js/main.0a9ba93f.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "0a9ba93f9acd24c36e97",
+    "url": "/static/css/main.0c0a606e.chunk.css"
+  },
+  {
+    "revision": "a1b5ebdd8af70abf9e9115b8bcc23419",
+    "url": "/index.html"
+  }
+];
